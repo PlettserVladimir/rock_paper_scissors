59f380fd461b78af3c48fff60c9b7f82fa25423c
vladimir <pletcervv@mail.ru>
2023-09-24 10:57:52 +0700

    Добавлено использование массива choices,добавлен цикл.

--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,32 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //final String [] choices ={"Камень","Ножницы","Бумага"};
+        final String [] choices ={"Камень","Ножницы","Бумага"};
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-        int compChoice = random.nextInt(3);//Генерирует случайный индекс от 0 до размера массива
 
-        System.out.println("Начало игры");
-        System.out.println("Выберите вариант");
-        System.out.println("""
-                0.Камень
-                1.Ножницы
-                2.Бумага
+        while (true) {
+            System.out.println("Выберите вариант:");
+            System.out.println("""
+                1.Камень
+                2.Ножницы
+                3.Бумага
+                4.Выход
                 """);
-        int playerChoice = scanner.nextInt();
-        System.out.println(compChoice);
-        if (playerChoice == compChoice){
-            System.out.println("Ничья");
-        } else if (playerChoice == 0 && compChoice == 1 || playerChoice ==1 && compChoice == 2 || playerChoice == 2 && compChoice == 0) {
-            System.out.println("Победил игрок");
-        }else {
-            System.out.println("Победил компьютер");
+            int playerChoice = scanner.nextInt() - 1;
+            if (playerChoice == 3) {
+                break;
+            }
+            int compChoice = random.nextInt(3);//Генерирует случайный индекс от 0 до размера массива
+                System.out.println("Выбор игрока: " + choices[playerChoice] + "<->" + "Выбор компьютера: " + choices[compChoice]);
+                if (playerChoice == compChoice) {
+                    System.out.println("Ничья");
+                } else if (playerChoice == 0 && compChoice == 1 || playerChoice == 1 && compChoice == 2 || playerChoice == 2 && compChoice == 0) {
+                    System.out.println("Победил игрок");
+                } else {
+                    System.out.println("Победил компьютер");
+                }
         }
+
     }
 }
